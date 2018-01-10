d4a04f9fc5b54c7c5d42dba7146a0a642eefbd0e
jinjirosan <rayflinkerbusch@Atlas.local>
2018-01-10 19:58:29 +0100

    rewrite 3.2.3-REFACTOR 0.8

--- vegasmonitor-initialstate.py
+++ vegasmonitor-initialstate.py
@@ -1,24 +1,24 @@
 # /usr/bin/env python
 #
 # Maintainer 	: JinjiroSan
-# Version	: vegasmonitor 2.0 - initialstate_streamer - rewrite 3.2.3-REFACTOR 0.7
+# Version	: vegasmonitor 2.0 - initialstate_streamer - rewrite 3.2.3-REFACTOR 0.8
 
-import os                                                      ## system terminal access
-import sys                                                     ## for the exit routine
-import time                                                    ## import time for sleeping
-import threading                                               ## import threading
-import traceback                                               ## import traceback for exception handling
-import glob                                                    ## used for pathnames pattern matching
-import psutil                                                  ## used for system monitoring
-import dweepy                                                  ## for alerting to dweet.io
-import vegasconfig                                             ## API key & secrets
-from gps import *                                              ## import the gps module
-from subprocess import PIPE, Popen                             ## to get the cpu temp from file
-from envirophat import light, weather, motion, analog          ## the meat and potato of sensors
-from ISStreamer.Streamer import Streamer                       ## getting stuff to initial state
-from geopy.geocoders import Nominatim                          ## for physical address translation
-from geopy.exc import GeocoderTimedOut                         ## geo error handling
-from bisect import bisect_left                                 ## for chance of rain from pressure value
+import os  ## system terminal access
+import sys  ## for the exit routine
+import time  ## import time for sleeping
+import threading  ## import threading
+import traceback  ## import traceback for exception handling
+import glob  ## used for pathnames pattern matching
+import psutil  ## used for system monitoring
+import dweepy  ## for alerting to dweet.io
+import vegasconfig  ## API key & secrets
+from gps import *  ## import the gps module
+from subprocess import PIPE, Popen  ## to get the cpu temp from file
+from envirophat import light, weather, motion, analog  ## the meat and potato of sensors
+from ISStreamer.Streamer import Streamer  ## getting stuff to initial state
+from geopy.geocoders import Nominatim  ## for physical address translation
+from geopy.exc import GeocoderTimedOut  ## geo error handling
+from bisect import bisect_left  ## for chance of rain from pressure value
 
 geolocator = Nominatim()
 gpsd = gps(mode=WATCH_ENABLE)
@@ -47,16 +47,17 @@ base_dir_headliner = '/sys/bus/w1/devices/28-041658f014ff/w1_slave'
 thingid = 'vegasmonitor_2_0_stokke'
 mydweet = {}
 
+
 ## function galore
 
 def read_enviro_temp():
     temp_raw = round(weather.temperature(), 2)
     temp_cpu = get_cpu_temperature()
-    temp_calibrated = temp_raw - ((temp_cpu - temp_raw) / 0.7)     ## open air correction value 1.2 , in stokke 0.7
+    temp_calibrated = temp_raw - ((temp_cpu - temp_raw) / 0.7)  ## open air correction value 1.2 , in stokke 0.7
     return temp_calibrated
 
 def read_enviro_heading():
-    corr_heading = (motion.heading() - 10) % 360        ## verify North correction , 10 is a placeholder
+    corr_heading = (motion.heading() - 10) % 360  ## verify North correction , 10 is a placeholder
     e_heading = corr_heading
     return e_heading
 
@@ -65,17 +66,16 @@ def read_enviro_accelerometer():
     return axes
 
 class weatherdata():
-    def __init__(self):
-        ## KNMI historical pressure-rainchance data
-        self.raintable = {0: 80, 994: 70, 998: 60, 1002: 50, 1007: 40, 1011: 30, 1016: 20, 1020: 10}
 
-    def read_temp_raw_headliner(self):
+    @staticmethod
+    def read_temp_raw_headliner():
         f_headliner = open(base_dir_headliner, 'r')
         lines_headliner = f_headliner.readlines()
         f_headliner.close()
         return lines_headliner
 
-    def read_temp_headliner(self):
+    @staticmethod
+    def read_temp_headliner():
         lines_headliner = self.read_temp_raw_headliner()
         while lines_headliner[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
@@ -86,19 +86,25 @@ class weatherdata():
             temp_c_headliner = float(temp_string_headliner) / 1000.0
             return temp_c_headliner
 
-    def read_enviro_pressure(self):
+    @staticmethod
+    def read_enviro_pressure():
         e_pressure = round(weather.pressure(), 2)
         return e_pressure * 0.01
 
-    def rainchance(self, pressure):
-        return self.raintable[max({k: self.raintable[k] for k in self.raintable if k < read_enviro_pressure()})]
+    @staticmethod
+    def rainchance(pressure):
+        raintable = {0: 80, 994: 70, 998: 60, 1002: 50, 1007: 40, 1011: 30, 1016: 20, 1020: 10}
+        return raintable[max({k: raintable[k] for k in raintable if k < weatherdata.read_enviro_pressure()})]
 
-    def read_enviro_light(self):
+    @staticmethod
+    def read_enviro_light():
         e_light = light.light()
         return e_light
 
+
 class sysmon():
 
+    @staticmethod
     def rpisystem():
         disk = psutil.disk_usage('/')
         cpu_percent = psutil.cpu_percent(percpu=False)
@@ -107,32 +113,34 @@ class sysmon():
         mem_percent_used = mem.percent
         return disk_percent_used, cpu_percent, mem_percent_used
 
+    @staticmethod
     def get_cpu_temperature():
         process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
         output, _error = process.communicate()
         return float(output[output.index('=') + 1:output.rindex("'")])
 
-def collect_env_data():                     ## dict must contain all the sanatized clean data to transmit
-    temp_cpu = get_cpu_temperature()
+def collect_env_data():  ## dict must contain all the sanitized clean data to transmit
+
+    temp_cpu = sysmon.get_cpu_temperature()
     temp_raw = round(weather.temperature(), 2)
     temp = round(read_enviro_temp(), 2)
-    heading = round(read_enviro_heading(), 0)
+    heading = round(weatherdata.read_enviro_heading(), 0)
     axes = read_enviro_accelerometer()
-    pressure = round(read_enviro_pressure(), 1)
-    rainchancepercentage = rainchance(pressure)
-    e_light = read_enviro_light()
-    temp_c_headliner = round(read_temp_headliner(), 2)
+    pressure = round(weatherdata.read_enviro_pressure(), 1)
+    rainchancepercentage = weatherdata.rainchance(pressure)
+    e_light = weatherdata.read_enviro_light()
+    temp_c_headliner = round(weatherdata.read_temp_headliner(), 2)
     temp_f_headliner = temp_c_headliner * 9.0 / 5.0 + 32.0
-    x = round(axes[0], 1) * 100 - 40         ## correction values at the end to compensate mounting position
-    y = round(axes[1], 1) * 100 + 70         ## round to one decimal place, multiply by 100 for degrees
+    x = round(axes[0], 1) * 100 - 40  ## correction values at the end to compensate mounting position
+    y = round(axes[1], 1) * 100 + 70  ## round to one decimal place, multiply by 100 for degrees
     z = round(axes[2], 1) * 100 + 60
-    dict_env_data = {'rpi temperature(C)': temp_cpu, 'enviro temperature(C) uncalibrated': temp_raw, 'enviro temperature(C) calibrated': temp, 'enviro heading': heading, 'enviro accelerometer X (roll)': x, 'enviro accelerometer Y (pitch)': y, 'enviro accelerometer Z (yaw)': z, 'enviro pressure(hPa)': pressure, 'chance of rain(%)': rainchancepercentage, 'enviro light intensity': e_light, 'Coordinates': Coordinates, 'Reported Time': gpsd.fix.time, 'Altitude (m)': gpsd.fix.altitude, 'Climb (m/s)': gpsd.fix.climb, 'Speed (m/s)': gpsd.fix.speed, 'GPS mode': gpsd.fix.mode}
+    dict_env_data = {'rpi temperature(C)': temp_cpu, 'enviro temperature(C) uncalibrated': temp_raw, 'enviro temperature(C) calibrated': temp, 'enviro heading': heading, 'enviro accelerometer X (roll)': x, 'enviro accelerometer Y (pitch)': y, 'enviro accelerometer Z (yaw)': z, 'enviro pressure(hPa)': pressure, 'chance of rain(%)': rainchancepercentage, 'enviro light intensity': e_light, 'Coordinates': coordinates, 'Reported Time': gpsd.fix.time, 'Altitude (m)': gpsd.fix.altitude, 'Climb (m/s)': gpsd.fix.climb, 'Speed (m/s)': gpsd.fix.speed, 'GPS mode': gpsd.fix.mode}
     return
 
 class transmit():
 
     def send_to_dweet():
-        mydweet = {}                      ## clear any old data in dweet.io table
+        mydweet = {}  ## clear any old data in dweet.io table
         mydweet['sender'] = 'stokkezero'  ## add identifier
         ## send table to dweet.io
         dweepy.dweet_for(thingid, dict_env_data)  ## removed semicolon from end due to pep8, see if this correct.
@@ -141,25 +149,27 @@ class transmit():
     def send_to_initialstate():
         return
 
+
 ##  thread to collect gpsd info
 class GpsPoller(threading.Thread):
 
-    def __init__(self):
-        threading.Thread.__init__(self)
-        global gpsd                         ## bring gpsd in scope
-        gpsd = gps(mode=WATCH_ENABLE)       ## start the stream of info
-        self.current_value = None           ## clear
-        self.running = True                 ## setting the thread running to true for the loop
+    def _init_(self):
+        threading.Thread._init_(self)
+        global gpsd  ## bring gpsd in scope
+        gpsd = gps(mode=WATCH_ENABLE)  ## start the stream of info
+        self.current_value = None  ## clear
+        self.running = True  ## setting the thread running to true for the loop
 
     def run(self):
         global gpsd
         while gpsp.running:
             gpsd.next()
 
+
 def main():
-    gpsp = GpsPoller()                    ## create the thread
+    gpsp = GpsPoller()  ## create the thread
     try:
-        gpsp.start()                      ## start and storing data in self.current_value
+        gpsp.start()  ## start and storing data in self.current_value
         while True:
             gpsd.next()
             os.system('clear')
@@ -169,7 +179,7 @@ def main():
 
             ## Sending the data to initial state and populate dweet.io table
             streamer.log("Coordinates", coordinates)
-            if coordinates == "nan,nan":        ## dweet barfs on non-existing coords, so make sure values are numerical
+            if coordinates == "nan,nan":  ## dweet barfs on non-existing coords, so make sure values are numerical
                 mydweet['latitude'] = 0
                 mydweet['longitude'] = 0
             else:
@@ -230,7 +240,8 @@ def main():
                 noaddress = "no location data - no gps coords - probably indoors"
                 streamer.log("Location", noaddress)
             else:
-                location = geolocator.reverse(coordinates, timeout=10)  ## reverse geocode coordinates
+                location = geolocator.reverse(coordinates,
+                                              timeout=10)  ## reverse geocode coordinates
                 streamer.log("Location", location.address)
     ## if the geocoder times out, stream a message and keep looping
     except GeocoderTimedOut as e:
@@ -238,11 +249,14 @@ def main():
         pass
 
     ## user interupt or system exits by itself, print a message, close
-    except (KeyboardInterrupt, SystemExit):         ## user interupt CTRL-C
-        print "\nKilling Thread..."
+    except (KeyboardInterrupt, SystemExit):  ## user interupt CTRL-C
+        print
+        "\nKilling Thread..."
         gpsp.running = False
-        gpsp.join()                             ## wait for the thread to finish what it's doing
-        print "Done.\nExiting."
+        gpsp.join()  ## wait for the thread to finish what it's doing
+        print
+        "Done.\nExiting."
 
-if __name__ == '__main__':
+
+if _name_ == '_main_':
     main()
